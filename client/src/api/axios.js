2e1046d82c62992a0049923d36a43eb8642ae9ce
prasannayeluguri <yeluguriprasanna@gmail.com>
2026-04-18 16:17:02 +0530

    backend url deployed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"https://mern-e-commers-647o.onrender.com"
+    baseURL:"https://mern-e-commers-647o.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
